8f0d615eda8c5b37970ad640c44a376fba6724b3
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-25 21:48:15 +0200

    Initial Commit

---
+++ MovieDBApp/MovieDBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBAppApp.swift
+//  MovieDBApp
+//
+//  Created by admin on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
