2702b1f7f5e8ce04a8614128519244ca5fc96877
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-26 18:59:34 +0200

    feat
    added color sets, constants and imageloader using SDWebImage SDK

---
+++ MovieDBApp/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MovieDBApp
+//
+//  Created by admin on 26/07/2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let mockImage = "https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg"
+    static let imageGet = "https://image.tmdb.org/t/p/w500"
+}
