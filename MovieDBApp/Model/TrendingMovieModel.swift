b902b316f3cd5ad19adeba64b297367b75568500
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-26 20:15:55 +0200

    refactor
    created separate groups for viewmodel and model

---
+++ MovieDBApp/Model/TrendingMovieModel.swift
@@ -0,0 +1,52 @@
+//
+//  TrendingMovieModel.swift
+//  MovieDBApp
+//
+//  Created by admin on 26/07/2024.
+//
+
+import Foundation
+
+struct TrendingResults: Codable {
+    let page: Int
+    let results: [TrendingMovieModel]
+    let total_pages: Int
+    let total_results: Int
+}
+
+struct TrendingMovieModel: Identifiable, Codable {
+    let id: Int
+    let title: String
+    let description: String
+    let posterPath: String
+    let adult: Bool
+    let releaseDate: String
+    let voteAverage: Double
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case description = "overview"
+        case posterPath = "poster_path"
+        case adult
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+    }
+    
+    var fullPosterPath: String {
+            Constants.imageGet + posterPath
+        }
+    
+    static var mock: TrendingMovieModel {
+        TrendingMovieModel(
+            id: 123,
+            title: "Deadpool & Wolverine",
+            description: "some description",
+            posterPath: "123452525",
+            adult: true,
+            releaseDate: "26/07/2024",
+            voteAverage: 8.2
+        )
+    }
+    
+}
