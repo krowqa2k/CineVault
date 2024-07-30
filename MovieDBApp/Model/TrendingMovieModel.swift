25749172d4dc49d49818f3a03984618d94c60128
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-30 22:00:28 +0200

    minor change
    changed description to mock data for trending movie

--- MovieDBApp/Model/TrendingMovieModel.swift
+++ MovieDBApp/Model/TrendingMovieModel.swift
@@ -41,7 +41,7 @@ struct TrendingMovieModel: Identifiable, Codable {
         TrendingMovieModel(
             id: 123,
             title: "Deadpool & Wolverine",
-            description: "some description",
+            description: "Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
             posterPath: "123452525",
             adult: true,
             releaseDate: "26/07/2024",
