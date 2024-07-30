27c0b3da008e7520905051c88473425218c76b69
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-30 21:59:40 +0200

    feat
    added detail views for movies and actors

---
+++ MovieDBApp/Screens/DetailScreens/PopularActorDetailView.swift
@@ -0,0 +1,88 @@
+//
+//  PopularActorDetailView.swift
+//  MovieDBApp
+//
+//  Created by admin on 30/07/2024.
+//
+
+import SwiftUI
+
+struct PopularActorDetailView: View {
+    var imageName: String = Constants.mockImage
+    var actor: PopularActorModel = .mock
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.blackDB.ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 0) {
+                ImageLoader(imageURL: imageName).ignoresSafeArea(edges: .top)
+                    .frame(height: UIScreen.main.bounds.height / 1.7, alignment: .top)
+                    .overlay (
+                        VStack(alignment: .leading) {
+                            VStack(alignment: .leading) {
+                                Text(actor.name)
+                                    .font(.title)
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.white)
+                                    .padding(.horizontal)
+                                
+                                if actor.gender == 1 {
+                                    Text("Gender: Female")
+                                        .font(.subheadline)
+                                        .fontWeight(.medium)
+                                        .foregroundStyle(.white)
+                                        .padding(.horizontal)
+                                } else {
+                                    Text("Gender: Male")
+                                        .font(.subheadline)
+                                        .fontWeight(.medium)
+                                        .foregroundStyle(.white)
+                                        .padding(.horizontal)
+                                }
+                            }
+                        }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .frame(maxHeight: 100)
+                            .background(
+                                LinearGradient(colors: [Color.blackDB.opacity(0.001), Color.blackDB.opacity(1)], startPoint: .top, endPoint: .bottom)
+                            )
+                        ,alignment: .bottom
+                    )
+                Text("Known for: ")
+                    .font(.title3)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal)
+                    .padding(.bottom, 2)
+                ForEach(actor.knownFor) { movie in
+                    Text("\(movie.title ?? ""),")
+                }
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.leading)
+            }
+            .overlay(
+                Circle()
+                    .frame(width: 40, height: 40)
+                    .foregroundStyle(.blackDB.opacity(0.8))
+                    .overlay(
+                        Image(systemName: "xmark")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.white)
+                    )
+                    .padding()
+                    .onTapGesture {
+                        dismiss()
+                    },alignment: .topLeading
+            )
+            .frame(maxHeight: .infinity, alignment: .top)
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+}
+
+#Preview {
+    PopularActorDetailView()
+}
---
+++ MovieDBApp/Screens/DetailScreens/UpcomingMovieDetailView.swift
@@ -0,0 +1,76 @@
+//
+//  UpcomingMovieDetailView.swift
+//  MovieDBApp
+//
+//  Created by admin on 30/07/2024.
+//
+
+import SwiftUI
+
+struct UpcomingMovieDetailView: View {
+    var imageName: String = Constants.mockImage
+    var movie: UpcomingMovieModel = .mock
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.blackDB.ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 0) {
+                ImageLoader(imageURL: imageName).ignoresSafeArea(edges: .top)
+                    .frame(height: UIScreen.main.bounds.height / 1.7, alignment: .top)
+                    .overlay (
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(movie.adult ? "18+" : "")
+                                    .frame(width: 40, height: 40)
+                                    .font(.headline)
+                                    .foregroundStyle(.blackDB)
+                                    .background(movie.adult ? Color.red : .clear)
+                                    .cornerRadius(12)
+                                Spacer()
+                            }
+                            .padding(.horizontal)
+                            
+                            Text(movie.title)
+                                .font(.title)
+                                .fontWeight(.medium)
+                                .foregroundStyle(.white)
+                                .padding(.horizontal)
+                        }
+                            .frame(maxWidth: .infinity)
+                            .frame(maxHeight: 100)
+                            .background(
+                                LinearGradient(colors: [Color.blackDB.opacity(0.001), Color.blackDB.opacity(1)], startPoint: .top, endPoint: .bottom)
+                            )
+                        ,alignment: .bottom
+                    )
+                Text(movie.overview)
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.leading)
+            }
+            .overlay(
+                Circle()
+                    .frame(width: 40, height: 40)
+                    .foregroundStyle(.blackDB.opacity(0.8))
+                    .overlay(
+                        Image(systemName: "xmark")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.white)
+                    )
+                    .padding()
+                    .onTapGesture {
+                        dismiss()
+                    },alignment: .topLeading
+            )
+            .frame(maxHeight: .infinity, alignment: .top)
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+}
+
+#Preview {
+    UpcomingMovieDetailView()
+}
