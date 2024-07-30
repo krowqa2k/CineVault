d35dad21c1a71cf1883d8f6661e78c0cb2bde53a
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-30 14:54:04 +0200

    feat
    created mock data for actor and known for models

--- MovieDBApp/Model/PopularActorsModel.swift
+++ MovieDBApp/Model/PopularActorsModel.swift
@@ -18,7 +18,7 @@ struct PopularActorModel: Codable, Identifiable {
     let name, originalName: String
     let popularity: Double
     let profilePath: String
-    let knownFor: [KnownFor]
+    let knownFor: [KnownForModel]
 
     enum CodingKeys: String, CodingKey {
         case adult, gender, id
@@ -32,18 +32,39 @@ struct PopularActorModel: Codable, Identifiable {
     var fullPosterPath: String {
             Constants.imageGet + profilePath
         }
+    
+    static var mock: PopularActorModel {
+        PopularActorModel(
+            adult: false,
+            gender: 2,
+            id: 64,
+            name: "Gary Oldman",
+            originalName: "Gary Oldman",
+            popularity: 221.628,
+            profilePath: "/2v9FVVBUrrkW2m3QOcYkuhq9A6o.jpg",
+            knownFor: [KnownForModel(
+                id: 155,
+                title: "The Dark Knight",
+                originalTitle: "The Dark Knight",
+                overview: "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.",
+                posterPath: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+                adult: false,
+                popularity: 168.441,
+                releaseDate: "2008-07-16",
+                voteAverage: 8.516
+            )]
+        )
+    }
 }
 
-struct KnownFor: Codable, Identifiable {
+struct KnownForModel: Codable, Identifiable {
     let id: Int
     let title, originalTitle: String?
     let overview, posterPath: String
     let adult: Bool
     let popularity: Double
     let releaseDate: String?
-    let video: Bool?
     let voteAverage: Double
-    let name, originalName, firstAirDate: String?
 
     enum CodingKeys: String, CodingKey {
         case id, title
@@ -53,16 +74,26 @@ struct KnownFor: Codable, Identifiable {
         case adult
         case popularity
         case releaseDate = "release_date"
-        case video
         case voteAverage = "vote_average"
-        case name
-        case originalName = "original_name"
-        case firstAirDate = "first_air_date"
     }
     
     var fullPosterPath: String {
             Constants.imageGet + posterPath
         }
+    
+    static var mock: KnownForModel {
+        KnownForModel(
+            id: 155,
+            title: "The Dark Knight",
+            originalTitle: "The Dark Knight",
+            overview: "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.",
+            posterPath: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+            adult: false,
+            popularity: 168.441,
+            releaseDate: "2008-07-16",
+            voteAverage: 8.516
+        )
+    }
 }
 
 
