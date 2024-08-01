affbb3f37437bf9fc3c0ff4090302c7ca0033601
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-01 17:42:57 +0200

    feat
    created detail views for series

---
+++ MovieDBApp/Screens/DetailScreens/PopularSeriesDetailView.swift
@@ -0,0 +1,87 @@
+//
+//  PopularSeriesDetailView.swift
+//  MovieDBApp
+//
+//  Created by Mateusz Krówczyński on 01/08/2024.
+//
+
+import SwiftUI
+
+struct PopularSeriesDetailView: View {
+    var imageName: String = Constants.mockImage
+    var movie: PopularSeriesModel = .mock
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            Color.blackDB.ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 0) {
+                ImageLoader(imageURL: imageName).ignoresSafeArea(edges: .top)
+                    .frame(height: UIScreen.main.bounds.height / 1.7, alignment: .top)
+                    .overlay (
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(movie.adult ? "18+" : "")
+                                    .frame(width: 40, height: 40)
+                                    .font(.headline)
+                                    .foregroundStyle(.blackDB)
+                                    .background(movie.adult ? Color.red : .clear)
+                                    .cornerRadius(12)
+                                Spacer()
+                                HStack {
+                                    Image(systemName: "star.fill")
+                                        .font(.headline)
+                                        .foregroundStyle(.yellow)
+                                    Text("\(movie.voteAverage.formatted())")
+                                        .font(.headline)
+                                        .foregroundStyle(.yellow)
+                                }
+                                .padding(4)
+                                .background(Color.blackDB)
+                                .cornerRadius(12)
+                            }
+                            .padding(.horizontal)
+                            
+                            Text(movie.name)
+                                .font(.system(size: 26))
+                                .fontWeight(.medium)
+                                .foregroundStyle(.white)
+                                .padding(.horizontal)
+                        }
+                            .frame(maxWidth: .infinity)
+                            .frame(maxHeight: 100)
+                            .background(
+                                LinearGradient(colors: [Color.blackDB.opacity(0.001), Color.blackDB.opacity(1)], startPoint: .top, endPoint: .bottom)
+                            )
+                        ,alignment: .bottom
+                    )
+                Text(movie.overview)
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.leading)
+            }
+            .overlay(
+                Circle()
+                    .frame(width: 40, height: 40)
+                    .foregroundStyle(.blackDB.opacity(0.8))
+                    .overlay(
+                        Image(systemName: "xmark")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.white)
+                    )
+                    .padding()
+                    .onTapGesture {
+                        dismiss()
+                    },alignment: .topLeading
+            )
+            .frame(maxHeight: .infinity, alignment: .top)
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+}
+
+#Preview {
+    PopularSeriesDetailView()
+}
