1973a5762d71d889bccab4aaa674b0522e293dc0
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-01 16:15:29 +0200

    feat
    created model and updated viewmodel for toprated series

---
+++ MovieDBApp/Model/SeriesModels/TopRatedSeriesModel.swift
@@ -0,0 +1,47 @@
+//
+//  TopRatedSeriesModel.swift
+//  MovieDBApp
+//
+//  Created by Mateusz Krówczyński on 01/08/2024.
+//
+
+import Foundation
+
+struct TopRatedSeriesResults: Codable {
+    let page: Int
+    let results: [TopRatedSeriesModel]
+}
+
+struct TopRatedSeriesModel: Codable, Identifiable {
+    let adult: Bool
+    let id: Int
+    let overview: String
+    let posterPath, name: String
+    let voteAverage: Double
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case id
+        case overview
+        case posterPath = "poster_path"
+        case name
+        case voteAverage = "vote_average"
+    }
+    
+    var fullPosterPath: String {
+            Constants.imageGet + posterPath
+        }
+    
+    static var mock: TopRatedSeriesModel {
+        TopRatedSeriesModel(
+            adult: false,
+            id: 12345,
+            overview: "bald guy doing meth",
+            posterPath: "some poster path",
+            name: "Breaking Bad",
+            voteAverage: 8.626
+        )
+    }
+}
+
+
--- MovieDBApp/ViewModels/MovieDBViewModel.swift
+++ MovieDBApp/ViewModels/MovieDBViewModel.swift
@@ -14,6 +14,7 @@ class MovieDBViewModel: ObservableObject {
     @Published var popularActor: [PopularActorModel] = []
     @Published var airingToday: [AiringTodayModel] = []
     @Published var popularSeries: [PopularSeriesModel] = []
+    @Published var topRatedSeries: [TopRatedSeriesModel] = []
     @Published var upcoming: [UpcomingMovieModel] = [] {
         didSet {
             DispatchQueue.main.async {
@@ -40,6 +41,7 @@ class MovieDBViewModel: ObservableObject {
         getPopularActorData()
         getAiringTodayData()
         getPopularSeriesData()
+        getTopRatedSeriesData()
         sortUpcomingMoviesByDate()
         sortTopRatedMoviesByRating()
     }
@@ -296,6 +298,42 @@ class MovieDBViewModel: ObservableObject {
                 do {
                     let decodedSeries = try JSONDecoder().decode(PopularSeriesResults.self, from: data)
                     self?.popularSeries = decodedSeries.results
+                } catch {
+                    print("Decoding error \(error)")
+                }
+            }
+        }
+        .resume()
+    }
+    
+    func getTopRatedSeriesData() {
+        guard let url = URL(string: "https://api.themoviedb.org/3/tv/top_rated?api_key=\(MovieDBViewModel.api_key)") else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("No data.")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                print("Invalid response.")
+                return
+            }
+            
+            guard response.statusCode >= 200 && response.statusCode < 300 else {
+                print("Status code should be 2xx, but is \(response.statusCode)")
+                return
+            }
+            
+            guard error == nil else {
+                print("Error: \(String(describing: error))")
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                do {
+                    let decodedSeries = try JSONDecoder().decode(TopRatedSeriesResults.self, from: data)
+                    self?.topRatedSeries = decodedSeries.results
                 } catch {
                     print("Decoding error \(error)")
                 }
