b2329ff0ad46298c7290b94681e43608e833c3cd
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-02 18:36:27 +0200

    feat
    added Search View

---
+++ MovieDBApp/Screens/Main/SearchView .swift
@@ -0,0 +1,18 @@
+//
+//  SearchView .swift
+//  MovieDBApp
+//
+//  Created by Mateusz Krówczyński on 02/08/2024.
+//
+
+import SwiftUI
+
+struct SearchView_: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SearchView_()
+}
