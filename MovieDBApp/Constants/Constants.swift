39e87b6716387dcec46b617e6dd72d5be9e30b54
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-03 14:25:17 +0200

    feat
    added noImage constant for searching purpose

--- MovieDBApp/Constants/Constants.swift
+++ MovieDBApp/Constants/Constants.swift
@@ -10,4 +10,5 @@ import Foundation
 struct Constants {
     static let mockImage = "https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg"
     static let imageGet = "https://image.tmdb.org/t/p/w500"
+    static let noImage = "https://static-00.iconduck.com/assets.00/no-image-icon-2048x2048-2t5cx953.png"
 }
