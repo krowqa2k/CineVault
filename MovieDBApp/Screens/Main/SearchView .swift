a51f1b609ce011b0123da1862ac2e0715815e685
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-03 14:24:29 +0200

    feat
    added full functioning Search View and DetailView for searched movie

--- MovieDBApp/Screens/Main/SearchView .swift
+++ MovieDBApp/Screens/Main/SearchView .swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct SearchView_: View {
+    @StateObject private var viewModel = MovieDBViewModel()
+    @State private var query: String = ""
+        
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.blackDB.ignoresSafeArea()
+            VStack(spacing: 8) {
+                searchBar
+                    .padding(.top)
+                Spacer()
+                moviesResult
+            }
+            .toolbar(.hidden, for: .navigationBar)
+        }
+        
+        
+    }
+    
+    private var searchBar: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .font(.title)
+                .foregroundStyle(.purpleDB)
+                .padding(.leading)
+                .padding(.trailing, 0)
+            TextField("Search movies", text: $query) {
+                viewModel.getSearchDBData(query: query)
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(.trailing,12)
+            .padding(.leading, 0)
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    private var moviesResult: some View {
+        ScrollView(.vertical) {
+            ForEach(viewModel.searchDB) { row in
+                NavigationLink(destination: SearchMovieDetailScreen(imageName: row.fullPosterPath, movie: row)) {
+                    HStack(spacing: 12) {
+                        ImageLoader(imageURL: row.fullPosterPath)
+                            .frame(width: 110, height: 160)
+                            .cornerRadius(16)
+                        VStack(alignment: .leading, spacing: 8){
+                            Text(row.title ?? "")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                                .multilineTextAlignment(.leading)
+                            Text(viewModel.getYear(from: row.releaseDate ?? ""))
+                                .font(.system(size: 14))
+                                .foregroundStyle(.gray)
+                        }
+                        .frame(maxHeight: 150, alignment: .top)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                }
+            }
+            .scrollIndicators(.hidden)
+        }
     }
 }
 
 #Preview {
-    SearchView_()
+    ZStack {
+        Color.blackDB.ignoresSafeArea()
+        SearchView_()
+    }
 }
